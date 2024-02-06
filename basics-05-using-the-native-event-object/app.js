4fc957c8805e852635d71c461d880816d9591a38
Evan Young <cantis@gmail.com>
2024-02-05 21:49:19 -0600

    Assignment 3 Works!

---
+++ basics-05-using-the-native-event-object/app.js
@@ -0,0 +1,62 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: '',
+      lastName: '',
+      // fullName: ''
+    };
+  },
+  watch:{
+    counter(value){
+      if (value > 50){
+        this.counter = 0;
+      }
+    }
+    // name(value) {
+    //   if (this.name === '') {
+    //     this.fullName = ''
+    //   } else {
+    //     this.fullName = value + ' ' + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === '') {
+    //     this.fullName = '';
+    //   } else {
+    //     this.fullName = this.name + ' ' + value;
+    //   }
+    // }
+  },
+  computed: {
+    fullName() {
+      if (this.name === '' || this.lastName === '') {
+        return '';
+      }
+      return this.name + ' ' + this.lastName;
+    }
+  },
+  methods: {
+    outputFullName() {
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Young';
+    },
+    setName(event, lastName) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+    resetInput() {
+      this.name = '';
+    }
+  }
+});
+
+app.mount('#events');
